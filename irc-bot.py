d6196e5661b1d9e842290fe2e04e1169729d3be3
ralsei38 <fontanatheo669@gmail.com>
2022-07-28 19:19:14 +0200

    can now connect authenticate, join a channel and talk to a user

--- irc-bot.py
+++ irc-bot.py
@@ -1,4 +1,6 @@
 import socket as s
+from time import sleep
+from datetime import datetime
 
 class IRC():
     def __init__(self, server, port) -> None:
@@ -7,29 +9,71 @@ class IRC():
         """
         self.socket = s.socket(s.AF_INET, s.SOCK_STREAM)
         self.socket.connect((server, port))
-    
+        print("socket receive:")
+        print(self.recv())
+
     def _is_nickname_taken(self) -> bool:
         raise  NotImplementedError
     
-    def authenticate(self, nickname, username) -> bool:
+    def authenticate(self, nickname) -> None:
         """
             following RFC1459 see 4.1.2 & 4.1.3 (4.1.1 being optional)
+            actually using username as nickname & vice-versa
         """
+        data = f"NICK {nickname} \r\n".encode("utf-8")
+        self.socket.send(data)
         
-        data = f"NICK ralsei"
-        pass
+        data = f"USER {nickname} 0 * :{nickname} \r\n".encode("utf-8")
+        self.socket.send(data)
+        print(self.recv())
+        print(self.recv())
+        print(self.recv())
 
-    def join_channel(self) -> bool:
-        pass
 
-    def talk(self, username) -> bool:
-        pass
+
+    def list_users(self) -> None:
+        print("LISTING USERS\n")
+        data = f"NAMES \r\n".encode("utf-8")
+        self.socket.send(data)
+        print(self.recv())
+        
+    def join_channel(self, channel) -> None:
+        if not channel.startswith('#'):
+            channel = '#'+channel
+        data = f"JOIN {channel} \r\n".encode("utf-8")
+        self.socket.send(data)
+        print(self.recv())
+        print(self.recv())
+        print(self.recv())
+
+    def recv(self) -> str:
+        answer = self.socket.recv(10**3)
+        if not answer:
+            raise Exception(f"No answer from {self.server}")
+        else:
+            return answer.decode("utf-8")
+
+    def send_pm(self, receiver, msg) -> bool:
+        """
+            4.4.1 Private messages
+        """
+        data = f"PRIVMSG {receiver} :{msg} \r\n".encode("utf-8")
+        self.socket.send(data)
+        print(self.recv())
     
-    def _receive(self) -> str:
-        pass
-    
-
 if __name__ == "__main__":
     server = "irc.root-me.org"
     port = 6667
+    nickname= f"rals-{str(datetime.now()).split('.')[-1]}"[:9]
+    channel=  "#root-me_challenge"
+
+    print(nickname)
+    print('*'*10)
     irc = IRC(server, port)
+    print('*'*10)
+    irc.authenticate(nickname)
+    print('*'*10)
+    irc.join_channel(channel)
+    print('*'*10)
+    irc.send_pm("nickname", "are you there ? thats a test btw")
+    print('*'*10)
